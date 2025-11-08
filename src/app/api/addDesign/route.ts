916e0bc3ef9c2bdc16c779836493fe900ad299af
Writefreak <iscribbledit@gmail.com>
2025-11-08 10:33:44 +0100

    fixed image upload

--- src/app/api/addDesign/route.ts
+++ src/app/api/addDesign/route.ts
@@ -1,16 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
-    const { title, description, imageUrl, uploadedBy } = await req.json();
+    const { title, description, imageUrl } = await req.json();
 
     if (!title || !imageUrl) {
       return NextResponse.json(
-        { error: "Title and Image Url are required" },
+        { error: "Title and Image URL are required" },
         { status: 400 }
       );
     }
@@ -22,11 +21,12 @@ export async function POST(req: NextRequest) {
         imageUrl,
       },
     });
+
     return NextResponse.json({ success: true, design }, { status: 201 });
   } catch (error: any) {
-    console.error("Error uploading design:", error);
+    console.error("❌ Error uploading design:", error);
     return NextResponse.json(
-      { error: "Failed to upload design" },
+      { error: error.message || "Failed to upload design" },
       { status: 500 }
     );
   }
